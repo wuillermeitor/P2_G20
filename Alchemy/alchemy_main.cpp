8c121063c2c85a057a828fca451de22f3877c91e
Guillermo Sánchez <guillermosanchezsanchez@enti.cat>
2017-04-30 00:44:32 +0200

    añadidas funciones introducidas en el main

--- Alchemy/alchemy_main.cpp
+++ Alchemy/alchemy_main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "alchemy.cpp"
 #include <vector>
 #include <set>
 #include <unordered_map>
@@ -7,9 +6,95 @@
 #include <string>
 #include <algorithm>
 #include <cstdlib>
+#include <Windows.h>
 
+std::vector<std::string> elementos;
+std::unordered_map<std::string, std::string> map;
 
-int main() {
+void add() {
+	int num;
+	std::string copia;
+	std::cin >> num;
+	copia = elementos[num];
+	elementos.push_back(copia);
+}
 
-	return 0;
+void addbasics() {
+	elementos.push_back("Air");
+	elementos.push_back("Earth");
+	elementos.push_back("Fire");
+	elementos.push_back("Water");
+}
+
+void delet() {
+	int num;
+	std::cin >> num;
+	elementos.erase(elementos.begin()+num);
+}
+
+void info() {
+	int num;
+	std::cin >> num;
+	std::string url;
+	url = "https://en.wikipedia.org/wiki/" + elementos[num];
+	ShellExecuteA(nullptr, "open", url.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+}
+
+void sort() {
+	std::sort(elementos.begin(), elementos.end());
+}
+
+void clean() {
+	std::sort(elementos.begin(), elementos.end());
+	elementos.erase(std::unique(elementos.begin(), elementos.end()), elementos.end());
+}
+
+void help() {
+	std::cout << "- Enter two numbers of your elements list to combine them." << std::endl;
+	std::cout << "- Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
+	std::cout << "- Enter 'add basics' to add new instances of the 4 basic elements." << std::endl;
+	std::cout << "- Enter the word 'delete' and the number of an element to erase it from your list." << std::endl;
+	std::cout << "- Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
+	std::cout << "- Enter the word 'sort' to sort by alphabetical order the elements in the list." << std::endl;
+	std::cout << "- Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
+	std::cout << "- Enter the word 'help' to show this tutorial." << std::endl;
+}
+
+void main() {
+	std::string written;
+	std::cin >> written;
+	while (written != "endgame") {
+		if (written == "add") {
+			add();
+		}
+		else if (written == "addbasics") {
+			addbasics();
+		}
+		else if (written == "delete") {
+			delet();
+		}
+		else if (written == "info") {
+			info();
+		}
+		else if (written == "sort") {
+			sort();
+		}
+		else if (written == "clean") {
+			clean();
+		}
+		else if (written == "help") {
+			help();
+		}
+		else {
+			std::cout << "and another one" << std::endl;
+		}
+
+		std::cout << "You have those elements :" << std::endl;
+
+		for (int i = 0; i < elementos.size(); i++) {
+			std::cout << i << ":  " << elementos[i] << std::endl;
+		}
+		std::cin >> written;
+		system("cls");
+	}
 }
